e4e69615fd860f265821eeae276e27cc872f7c88
Suppakit Neno <suppakit.neno@gmail.com>
2018-06-16 14:26:55 +0700

    test

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 4000;
-app.post('/webhook', (req, res) => res.sendStatus(200));
-app.listen(port);
-
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+const port = process.env.PORT || 4000
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.post('/webhook', (req, res) => {
+    let reply_token = req.body.events[0].replyToken
+    reply(reply_token)
+    res.sendStatus(200)
+})
+app.listen(port)
 console.log("App is running on " + port);
+
+function reply(reply_token) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {rRrdqJ81y+UadGyOdNhd2uc3jL2wTZ3Rf1GUN+ofv3DFGhHyXF60wtzwsYe+5eEb89s3qLUWjp3YuH7MhX43soH99Vg8L3fQ6d4vDAzvZrbnsGsXCUCy3J2UupJpv6AX6saoQrSJU4RDCG7mz+E12gdB04t89/1O/w1cDnyilFU=}'
+    }
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: 'Hello'
+        },
+        {
+            type: 'text',
+            text: 'How are you?'
+        }]
+    })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
+}
