416dab887c55c436efbabde20e5464faead033d1
Suppakit Neno <suppakit.neno@gmail.com>
2018-06-30 12:23:26 +0700

    a

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
+const path = require('path')
+const mysql = require('mysql')
 const app = express()
-const port = process.env.PORT || 4000
+const multer = require('multer')
+const hbs = require('hbs')
+const bcrypt = require('bcryptjs')
+const saltRounds = 10
+const cookieParser = require('cookie-parser')
+const expressValidator = require('express-validator')
+const flash = require('connect-flash')
+const session = require('express-session')
+const MySQLStore = require('express-mysql-session')(session)
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const moment = require('moment')
+var random = require("randomstring")
+//const port = process.env.PORT || 4000
+const port = 20000
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.post('/webhook', (req, res) => {
-    let reply_token = req.body.events[0].replyToken
-    reply(reply_token)
-    res.sendStatus(200)
-})
-app.listen(port)
-console.log("App is running on " + port);
+app.use(express.static(__dirname + '/eatemall/public'))
 
-function reply(reply_token) {
-    let headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer {rRrdqJ81y+UadGyOdNhd2uc3jL2wTZ3Rf1GUN+ofv3DFGhHyXF60wtzwsYe+5eEb89s3qLUWjp3YuH7MhX43soH99Vg8L3fQ6d4vDAzvZrbnsGsXCUCy3J2UupJpv6AX6saoQrSJU4RDCG7mz+E12gdB04t89/1O/w1cDnyilFU=}'
-    }
-    let body = JSON.stringify({
-        replyToken: reply_token,
-        messages: [{
-            type: 'text',
-            text: 'Hello'
-        },
-        {
-            type: 'text',
-            text: 'How are you?'
-        }]
-    })
-    request.post({
-        url: 'https://api.line.me/v2/bot/message/reply',
-        headers: headers,
-        body: body
-    }, (err, res, body) => {
-        console.log('status = ' + res.statusCode);
-    });
-}
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Eatemall2018",
+    database: "eatemall",
+    multipleStatements: true
+});
+
+con.connect(function(err) {
+    if(err) throw err
+    console.log("App is connected to database.")
+});
+
+app.listen(port)
+console.log("App is running on port: " + port);
